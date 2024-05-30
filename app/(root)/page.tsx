2a3f8539fe17c278ea489c04d110467f64f099dd
kyeongwxx <kyeongwxx@gmail.com>
2024-05-31 00:14:12 +0900

    feat : add dummy data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const page = async () => {
+const page = () => {
   return <div>HomePage</div>;
 };
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,10 @@ export function cn(...inputs: ClassValue[]) {
 export const sleep = (ms: number): Promise<void> => {
   return new Promise((res) => setTimeout(res, ms));
 };
+
+export function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
