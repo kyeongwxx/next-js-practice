b3b1aff70f59b94cdb46f80c1ffa5872fd2fe38b
kyeongwxx <kyeongwxx@gmail.com>
2024-06-01 15:49:40 +0900

    feat : sidebar - navigator

--- components/elements/Navigator.tsx
+++ components/elements/Navigator.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+'use client';
+
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+import { FiCompass, FiMusic } from 'react-icons/fi';
+import { GoHome } from 'react-icons/go';
 
 const Navigator = () => {
-  return <div>Navigator</div>;
+  const pathname = usePathname();
+
+  const routes = useMemo(() => {
+    return [
+      {
+        icon: <GoHome size={24} />,
+        label: '홈',
+        isActive: pathname === '/',
+        href: '/',
+      },
+      {
+        icon: <FiCompass size={24} />,
+        label: '둘러보기',
+        isActive: pathname === '/explore',
+        href: '/explore',
+      },
+      {
+        icon: <FiMusic size={24} />,
+        label: '보관함',
+        isActive: pathname === '/library',
+        href: '/library',
+      },
+    ];
+  }, [pathname]);
+
+  return (
+    <div>
+      <section className='flex flex-col gap-2 p-4'>
+        {routes.map((route) => (
+          <Link key={route.label} href={route.href}>
+            <div
+              className={cn(
+                'flex flex-row items-center gap-4 text-[16px] hover:bg-neutral-700 rounded-lg p-2',
+                route.isActive && 'bg-neutral-800'
+              )}
+            >
+              {route.icon}
+              <span>{route.label}</span>
+            </div>
+          </Link>
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default Navigator;
