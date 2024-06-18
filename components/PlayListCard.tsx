2a3b2748f21e367124e0e6b67b53a9b3e34c63ea
kyeongwxx <kyeongwxx@gmail.com>
2024-06-18 23:21:19 +0900

    feat : PlayListCard component inner more, play button

--- components/PlayListCard.tsx
+++ components/PlayListCard.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { getRandomElementFromArray } from '@/lib/utils';
-import { Playlist } from '@/types';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import { getRandomElementFromArray } from '@/lib/utils';
+import { Playlist } from '@/types';
+import { MdMoreVert } from 'react-icons/md';
+import { FiPlay } from 'react-icons/fi';
+import IconButton from './elements/IconButton';
 
 type PlayListCardProps = {
   playlist: Playlist;
@@ -21,10 +23,28 @@ const PlayListCard = ({ playlist }: PlayListCardProps) => {
     push(`/playlist?list=${id}`);
   };
 
+  const onClickPlay = () => {
+    // todo
+  };
+
   return (
-    <article className='h-[240px] cursor-pointer'>
+    <article className='h-[240px] cursor-pointer group'>
       <section onClick={onClickCard} className='relative h-[136px]'>
         <Image src={imgSrc} fill alt='thumbnail' className='object-cover' />
+        <div className='hidden relative group-hover:block bg-gradient-to-b from-[rgba(0,0,0,0.7)] top-0 w-full h-[136px]'>
+          <div className='absolute top-4 right-4'>
+            <IconButton
+              icon={<MdMoreVert size={20} />}
+              onClickIcon={() => console.log('clicked!!')}
+            />
+          </div>
+          <div
+            className='absolute bottom-4 right-4 flex items-center justify-center transform-gpu transition-transform hover:scale-110 bg-[rgba(0,0,0,0.7)] w-[45px] h-[45px] rounded-full hover:bg-[rgba(0,0,0,0.9)] pl-[2px]'
+            onClick={onClickPlay}
+          >
+            <FiPlay size={22} color='red' />
+          </div>
+        </div>
       </section>
 
       <section className='mt-2'>
