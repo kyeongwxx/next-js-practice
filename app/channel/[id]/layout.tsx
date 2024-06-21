4671ae92a2c52f382e086044a6dacfb8ff1a6b77
kyeongwxx <kyeongwxx@gmail.com>
2024-06-21 19:16:48 +0900

    feat : 각 페이지 layout header 적용

--- app/channel/[id]/layout.tsx
+++ app/channel/[id]/layout.tsx
@@ -1,11 +1,16 @@
-import React, { ReactNode } from 'react';
+import Header from '@/components/Header';
+import { ReactNode } from 'react';
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 const layout = ({ children }: LayoutProps) => {
-  return <div>{children}</div>;
+  return (
+    <div className='w-full h-full'>
+      <Header>{children}</Header>
+    </div>
+  );
 };
 
 export default layout;
